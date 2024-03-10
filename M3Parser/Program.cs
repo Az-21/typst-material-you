ef8efbe922afa130dc16af563b0afa9dc1ce3198
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-03-10 14:57:29 +0530

    Define workflow in `Main()`

--- M3Parser/Program.cs
+++ M3Parser/Program.cs
@@ -4,5 +4,8 @@ internal static class Program
 {
   static void Main()
   {
+    string[] files = Io.Input.GetFiles();
+    var themes = Parse.Theme.FromKotlinFiles(files);
+    Io.Output.WriteThemes(themes);
   }
 }
