8eda81938676efa67f32e7646fd2b639ba6d3edd
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-03-10 16:01:19 +0530

    Add version info in output file

--- M3Parser/Program.cs
+++ M3Parser/Program.cs
@@ -2,11 +2,12 @@
 
 internal static class Program
 {
+  private const string version = "1.0.0";
   static void Main()
   {
     string[] files = Io.Input.GetFiles();
     var themes = Parse.Theme.FromKotlinFiles(files);
-    Io.Output.WriteThemes(themes);
+    Io.Output.WriteThemes(themes, version);
 
     Console.Write("\nPress any key to exit...");
     Console.ReadKey();
